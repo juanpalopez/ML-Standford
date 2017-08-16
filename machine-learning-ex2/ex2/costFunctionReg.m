838eb460495cb64940a3d35bec88181173941586
Juanpa <juampalg85@gmail.com>
2017-08-16 01:43:45 -0500

    ex2 completed

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -18,12 +18,25 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+% Regularization terms
+reg_term = (lambda/(2*m)) * (theta(2:end)' * theta(2:end))
+gra_reg_tem = (lambda/m) * theta(2:end)
+
+% Hipotesis
 h_x = sigmoid(X * theta)
+
+% Cost for positive a negative labels
 cost_one = -y' * log(h_x)
 cost_zero =  (1 - y)' * log(1 - h_x)
 
-J = 1/m * (cost_one - cost_zero)
-grad = 1/m * ((h_x - y)'*X)'
+% Total Cost
+J = 1/m * (cost_one - cost_zero)  + reg_term
+
+%Gradient Descent
+grad_one = 1/m * ((h_x - y)'*X(:,1))'
+grad_else = 1/m * ((h_x - y)'*X(:,2:end))' + gra_reg_tem
+grad = [ grad_one;grad_else]
+
 
 
 
